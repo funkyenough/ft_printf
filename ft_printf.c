d7d68ef71dab4f4f5739e883627fe23e3a447443
funkyenough <14842981+funkyenough@users.noreply.github.com>
2024-09-16 01:25:18 +0900

    fix ft_format

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: yinhong <yinhong@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 13:30:06 by yinhong           #+#    #+#             */
-/*   Updated: 2024/09/15 16:25:09 by yinhong          ###   ########.fr       */
+/*   Updated: 2024/09/16 01:24:36 by yinhong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	ft_format(int fd, va_list args, int format)
+static int	ft_format(int fd, va_list *args, int format)
 {
 	int	print_len;
 
@@ -20,19 +20,19 @@ static int	ft_format(int fd, va_list args, int format)
 	if (format == '%')
 		print_len += ft_print_char(fd, '%');
 	else if (format == 'c')
-		print_len += ft_print_char(fd, va_arg(args, int));
+		print_len += ft_print_char(fd, va_arg(*args, int));
 	else if (format == 's')
-		print_len += ft_print_str(fd, va_arg(args, void *));
+		print_len += ft_print_str(fd, va_arg(*args, void *));
 	else if (format == 'p')
-		print_len += ft_print_ptr(fd, va_arg(args, void *));
+		print_len += ft_print_ptr(fd, va_arg(*args, void *));
 	else if (format == 'd' || format == 'i')
-		print_len += ft_print_nbr(fd, va_arg(args, int));
+		print_len += ft_print_nbr(fd, va_arg(*args, int));
 	else if (format == 'u')
-		print_len += ft_print_unbr(fd, va_arg(args, unsigned int));
+		print_len += ft_print_unbr(fd, va_arg(*args, unsigned int));
 	else if (format == 'x')
-		print_len += ft_print_hex(fd, va_arg(args, unsigned int), 0);
+		print_len += ft_print_hex(fd, va_arg(*args, unsigned int), 0);
 	else if (format == 'X')
-		print_len += ft_print_hex(fd, va_arg(args, unsigned int), 1);
+		print_len += ft_print_hex(fd, va_arg(*args, unsigned int), 1);
 	return (print_len);
 }
 
@@ -48,7 +48,7 @@ int	ft_printf(int fd, const char *str, ...)
 		if (*str == '%')
 		{
 			str++;
-			print_len += ft_format(fd, args, *str);
+			print_len += ft_format(fd, &args, *str);
 		}
 		else
 			print_len += ft_print_char(fd, *(const char *)str);
